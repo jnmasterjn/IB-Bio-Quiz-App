23b71122f2e4c403d5265624735902671f765c1c
Janice <janice0975751227@gmail.com>
2024-08-14 18:31:43 +0800

    create views.py and map them into urls.py

--- server/Trivia/api/views.py
+++ server/Trivia/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(["POST"])
+def login(request):
+    return Response({"login?"})
+
+
+@api_view(["POST"])
+def signup(request):
+    return Response({"signup?"})
+
+
+@api_view(["GET"])
+def test_token(request):
+    return Response({"token?"})
