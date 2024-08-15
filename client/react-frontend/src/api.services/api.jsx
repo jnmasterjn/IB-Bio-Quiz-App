14ac42f21d1da3a63d9eb8a9eb15147390ac9c79
Janice <janice0975751227@gmail.com>
2024-08-15 16:07:48 +0800

    axios stuff

---
+++ client/react-frontend/src/api.services/api.jsx
@@ -0,0 +1,14 @@
+//Trivia/client/react-frontend/src/api.services/api.jsx
+
+import axios from 'axios'
+
+const API_URL = 'http://127.0.0.1:8000/'
+
+export const signup = (username, password) => {
+    return axios.post(`${API_URL}/signup`, {username, password})
+}
+
+export const login = (username, password) => {
+    return axios.post(`${API_URL}/login`, {username, password})
+}
+
