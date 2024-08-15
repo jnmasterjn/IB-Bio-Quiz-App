70d85dc712da075d560cefb953cf191d8e0c3781
Janice <janice0975751227@gmail.com>
2024-08-15 15:22:30 +0800

    test token

--- server/Trivia/api/views.py
+++ server/Trivia/api/views.py
@@ -35,18 +35,30 @@ def signup(request):
 @api_view(["POST"])
 def login(request):
 
+    #Retrieve user based on provided username
     user = get_object_or_404(User, username = request.data['username'])
+
+    #check if the inserted password is correct, if not return error
     if not user.check_password(request.data['password']):
         return Response ({"error": "Invalid credentials"}, status= status.HTTP_400_BAD_REQUEST) 
     
+    #create or retrieve token for the user
     token, created = Token.objects.get_or_create(user=user)
 
+    #serialized user instances 
     serializer = UserSerializer(instance=user)
 
-
+    #return token and userdata in response
     return Response({"token": token.key, "user": serializer.data} )
 
 
-@api_view(["GET"])
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(["POST"])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+
 def test_token(request):
-    return Response({"token?"}) 
+    return Response("passed for {}".format(request.user.username))
