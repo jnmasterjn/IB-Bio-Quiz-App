596fe6f2ab519dbd8fec488b7f76587374dd9409
Janice <janice0975751227@gmail.com>
2024-08-17 16:54:09 +0800

    start database

--- server/Trivia/api/models.py
+++ server/Trivia/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Quiz(models.Model):
+    question = models.
+    optionOne = models.
+    optionTwo = models.
+    optionThree = models.
+
+class gameStats(models.Model):
+    user = models.ForeignKey(User,)
+    score = models.
