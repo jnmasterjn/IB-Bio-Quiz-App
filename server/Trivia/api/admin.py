fb87ac53bf49e5157639d9583ff7963574754719
Janice <janice0975751227@gmail.com>
2024-08-17 17:08:20 +0800

    import models into admin page

--- server/Trivia/api/admin.py
+++ server/Trivia/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+#put our models (databases) created in models.py into the admin page
+from api.models import Quiz, gameStats
+
+admin.site.register(Quiz)
+admin.site.register(gameStats)
--- server/Trivia/api/models.py
+++ server/Trivia/api/models.py
@@ -2,11 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Quiz(models.Model):
-    question = models.
-    optionOne = models.
-    optionTwo = models.
-    optionThree = models.
+    question = models.CharField(max_length=30)
+    optionOne = models.CharField(max_length=30)
+    optionTwo = models.CharField(max_length=30)
+    optionThree = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.question
 
 class gameStats(models.Model):
-    user = models.ForeignKey(User,)
-    score = models.
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    score = models.IntegerField()
+
+def __str__(self):
+        return self
