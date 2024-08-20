0e3c1d9afa4e39021f33cc929af2eadad70b7733
Janice <janice0975751227@gmail.com>
2024-08-20 15:28:10 +0800

    files structure, adding homepage, leaderboard

---
+++ client/react-frontend/src/assets/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+
+function Leaderboard(){
+    return(
+        <h1>Leaderboard</h1>
+    )
+}
+
+export default Leaderboard
--- client/react-frontend/src/main.jsx
+++ client/react-frontend/src/main.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import Signin from './assets/pages/Signin'
 import Signup from './assets/pages/Signup'
 import Game from './assets/pages/Game'
+import Leaderboard from './assets/pages/Dashboard'
+import Home from './assets/pages/Home'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './assets/authentication/Auth'
 import ProtectedRoute from './assets/authentication/ProtectRoute'
@@ -19,9 +21,19 @@ const myrouter = createBrowserRouter([
   },
 
   {
+    path:"Home",
+    element: <ProtectedRoute element ={<Home/>}/>
+  },
+
+  {
     path:"game",
     element: <ProtectedRoute element ={<Game/>}/>
-  }
+  },
+
+  {
+    path:"Leaderboard",
+    element: <ProtectedRoute element ={<Leaderboard/>}/>}
+  
 
 ])
 
