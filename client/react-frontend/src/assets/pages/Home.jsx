1a27c4cf52fb6bd4a9bfc3584e2adc16df04e23b
Janice <janice0975751227@gmail.com>
2024-08-20 16:00:10 +0800

    nav bar??

--- client/react-frontend/src/assets/pages/Home.jsx
+++ client/react-frontend/src/assets/pages/Home.jsx
@@ -1,7 +1,12 @@
+import {useNavigate, Link} from "react-router-dom";
 
 function Home(){
     return(
+        <>
         <h1>Home</h1>
+        <Link to='../game'><button>Game</button></Link>
+        <Link to='../leaderboard'><button>LeaderBoard</button></Link>
+        </>
     )
 }
 
