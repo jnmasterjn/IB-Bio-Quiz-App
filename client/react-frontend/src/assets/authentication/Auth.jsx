d4db87195377ff65806d99c6a357e292bf2e6ba5
Janice <janice0975751227@gmail.com>
2024-08-21 18:32:49 +0800

    add comments to auth.jsx

--- client/react-frontend/src/assets/authentication/Auth.jsx
+++ client/react-frontend/src/assets/authentication/Auth.jsx
@@ -4,6 +4,7 @@ const AuthContext = createContext(null)
 
 export const useAuth = () => useContext(AuthContext)
 
+//AuthProvider manage isLoggedIn with the useState
 export const AuthProvider = ({children}) => {
     
     const [isLoggedIn, setIsLoggedin] = useState(false)
@@ -17,9 +18,10 @@ export const AuthProvider = ({children}) => {
     }
 
     return(
-        <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+        <AuthContext.Provider value={{ isLoggedIn, login, logout }}> 
             {children}
         </AuthContext.Provider>
+        //When you call useAuth(), you get the isLoggedIn, login, and logout values from the context.
     )
 }
 
