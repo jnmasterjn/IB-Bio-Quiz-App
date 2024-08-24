b05908cb977d3021bd6dce15677ee34b6b1b8e4e
Janice <janice0975751227@gmail.com>
2024-08-25 00:49:07 +0800

    local storage stuff for login

--- client/react-frontend/src/assets/authentication/Auth.jsx
+++ client/react-frontend/src/assets/authentication/Auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect} from "react";
 
 const AuthContext = createContext(null)
 
@@ -7,13 +7,17 @@ export const useAuth = () => useContext(AuthContext)
 //AuthProvider manage isLoggedIn with the useState
 export const AuthProvider = ({children}) => {
     
-    const [isLoggedIn, setIsLoggedin] = useState(false)
+    const [isLoggedIn, setIsLoggedin] = useState(
+        localStorage.getItem('isLoggedIn'=== 'true')
+    )
 
     const login = () => {
         setIsLoggedin(true)
+        localStorage.setItem('isLoggedIn', 'true') //Store login state in localStorage
     }
 
     const logout = () => {
+        localStorage.removeItem('isLoggedIn')
         localStorage.removeItem('token')
         localStorage.removeItem('name') //remove name and token when logging out
         setIsLoggedin(false)
