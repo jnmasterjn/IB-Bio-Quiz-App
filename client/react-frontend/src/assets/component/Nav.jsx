478d7763f83b2c230ca3cc4424daf25ff27205eb
Janice <janice0975751227@gmail.com>
2024-11-16 23:22:20 +0800

    fix github

--- client/react-frontend/src/assets/component/Nav.jsx
+++ client/react-frontend/src/assets/component/Nav.jsx
@@ -19,7 +19,7 @@ return(
         <nav>
             <div>
                 <ul>
-                    <li><a href="/Home">Home...</a></li>
+                    <li><a href="/Home">Home</a></li>
                     <li><a href="/About">About</a></li>
                     <li><a href="/Contact">Contact</a></li>
                     <li><a href="/Review">Review</a></li>
